ff2a8bbb78a1dd7febe0d203ac025c0b6072a8bf
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 06:49:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ require 'pry'
 def speak_to_grandma(phrase)
   if phrase = "I lOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = phrase.upcase
+  elsif phrase != phrase.upcase
     "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
